b3f041d41aad85d81c12f48fc1cb8b3476cecb4d
mrtolkien <gary.mialaret@gmail.com>
2022-01-06 16:01:42 +0900

    Tests import rewritee

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,14 +1,13 @@
 import pytest
 from requests.models import HTTPError
 
-import lol_qq_parser.utils
-
 
 def test_working_endpoint():
     """
     Querying an arbitrary endpoint and making sure it works
     """
     # Using LPL 2021 Spring standings
+    import lol_qq_parser.utils
 
     standings = lol_qq_parser.utils.query_tjstats(
         "v1/compound/team?seasonId=148&stageIds=1%2C5"
@@ -24,6 +23,7 @@ def test_faulty_endpoint():
     Querying an endpoint that I cannot get and checking it fails as expected
     """
     # Trying to get seasons information
+    import lol_qq_parser.utils
 
     with pytest.raises(HTTPError):
         lol_qq_parser.utils.query_tjstats("v1/schedule/season?iOpen=-1")
