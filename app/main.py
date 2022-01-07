5a41bdf910d813b43dcf7fcc3dec298dd17a56e1
mrtolkien <gary.mialaret@gmail.com>
2022-01-07 18:05:03 +0900

    Added TODO for sources in the FastAPI app

--- app/main.py
+++ app/main.py
@@ -11,4 +11,5 @@ app = FastAPI(
 
 @app.get("/series")
 def get_series(match_id: int):
+    # TODO This does not return sources becaues of my godawful data format
     return lol_qq_parser.get_series_basic_info(match_id=match_id)
